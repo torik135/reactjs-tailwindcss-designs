302f7be42297cafeec73ec3930603c4ad28107f6
torik135 <riqibrekele@gmail.com>
2022-09-30 14:47:20 +0700

    add: Hero component

--- src/pages/bank/components/Hero/Hero.tsx
+++ src/pages/bank/components/Hero/Hero.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
 
-const Hero: React.FC = () => {
-  return <div>Hero</div>;
-};
+import { GetStarted } from '../';
+import styles from '../../style';
+import { discount, robot } from '../../assets';
+
+const Hero: React.FC = () => (
+  <section
+    id='home'
+    className={`flex md:flex-row flex-col ${styles.paddingY} `}
+  >
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[.9rem] mb-2'>
+        <img src={discount} alt='discount' className='w-[2rem] h-[2rem]' />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className='text-white'>20%</span> Discount for{' '}
+          <span className='text-white'>1 Month</span> Account
+        </p>
+      </div>
+
+      <div className='flex flex-row justify-between items-center w-full'>
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+          The Next <br className='sm:block hidden' />{' '}
+          <span className='text-gradient'>Generation</span>
+        </h1>
+        <div className='ss:flex hidden md:mr-4 mr-0'>
+          <GetStarted />
+        </div>
+      </div>
+
+      <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+        Payment Method
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses the methodology to identify the credits card
+        most likely to fit your needs. We examine annual percentage rates,
+        annual fees.
+      </p>
+    </div>
+
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img
+        src={robot}
+        alt='robot'
+        className='w-[100%] h-[100%] relative z-[5]'
+      />
+      <div className='absolute z-[0] w-[40%] h-[35%] top-[13rem] pink__gradient' />
+      <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+      <div className='absolute z-[0] w-[50%] h-[50%] rounded-full bottom-50 blue__gradient' />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted/>
+    </div>
+  </section>
+);
 
 export { Hero };
