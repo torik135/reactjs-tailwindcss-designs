dc9e72143681c5531e018495888112f68f2a5c38
torik135 <riqibrekele@gmail.com>
2022-10-01 15:29:23 +0700

    add: cta component

--- src/pages/bank/components/CTA/CTA.tsx
+++ src/pages/bank/components/CTA/CTA.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const CTA: React.FC = () => {
-  return <div>CTA</div>;
-};
+import styles from '../../style';
+import { Button } from '../';
+
+const CTA: React.FC = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-md box-shadow`}
+  >
+    <div className='flex flex-col flex-1'>
+      <h2 className={styles.heading2}>Let's try our service now</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Everything you need to accept card payment and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button styles='rounded-md'/>
+    </div>
+  </section>
+);
 
 export { CTA };
