1fade4cd30da31443dc8077873414e0bfa4efca2
torik135 <riqibrekele@gmail.com>
2022-10-01 14:36:48 +0700

    add: card deal component

--- src/pages/bank/components/CardDeal/CardDeal.tsx
+++ src/pages/bank/components/CardDeal/CardDeal.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const CardDeal: React.FC = () => {
-  return <div>CardDeal</div>;
-};
+import { card } from '../../assets';
+import styles, { layout } from '../../style';
+import { Button } from '../';
+
+const CardDeal: React.FC = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='sm:block hidden' /> in a few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning reward and saving money. But with hundreds of
+        credit card on the market.
+      </p>
+      <Button styles='mt-10 rounded-md'/>
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="cardeal-img" className='w-[100%] h-[100%]'/>
+    </div>
+  </section>
+);
 
 export { CardDeal };
