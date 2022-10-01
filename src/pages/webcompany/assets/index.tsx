8c7f1d366ac46e9cf667166f46b86f9460968238
torik135 <riqibrekele@gmail.com>
2022-10-01 16:49:38 +0700

    add import & export assets

---
+++ src/pages/webcompany/assets/index.tsx
@@ -0,0 +1,7 @@
+import hero from './img/hero.png';
+import css from './svg/css.svg';
+import html from './svg/html.svg';
+import js from './svg/js.svg';
+import node from './svg/node.svg';
+
+export { hero, css, html, js, node };
