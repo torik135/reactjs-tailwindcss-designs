059e66cfcbca33d76aff6e0c6e408e93a987eed6
torik135 <riqibrekele@gmail.com>
2022-10-01 15:19:55 +0700

    add: client component

--- src/pages/bank/components/Clients/Clients.tsx
+++ src/pages/bank/components/Clients/Clients.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Clients: React.FC = () => {
-  return <div>Clients</div>;
-};
+import { clients } from '../../constants';
+import styles from '../../style';
+
+const Clients: React.FC = () => (
+  <section id='clients' className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map(i => (
+        <div key={Math.random()} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+          <img src={i.logo} alt="clients-img" className='sm:w-[192px] w-[100px] object-contain hover:scale-110 hover:filter hover:invert'/>
+        </div>
+        ))}
+    </div>
+  </section>
+);
 
 export { Clients };
