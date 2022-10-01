bd52569b2416781eb864c5a016f89cc7104de5f5
torik135 <riqibrekele@gmail.com>
2022-10-01 16:49:48 +0700

    add constants quote

---
+++ src/pages/webcompany/constants/quote.ts
@@ -0,0 +1,14 @@
+export const quotes: string[] = [
+  {
+    text: 'WebCompany has always been there for me. Their Website are just like any other website but better.',
+    user: 'RandomUser123',
+  },
+  {
+    text: 'This is better than Loram Ipsum. YESS!!!!!!!!',
+    user: 'HarveySmithson666',
+  },
+  {
+    text: 'IT HAS COLOOOOORRRRRRR EEEEEEEEE!!!!!!!!!!',
+    user: 'NotHarveySmithson666',
+  },
+];
